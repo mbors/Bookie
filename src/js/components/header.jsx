9ade88f825ddee5cb91d201fc1d473fb14224e8f
Martyna Bors <martynabors@gmail.com>
2017-11-21 14:00:29 +0100

    react_initial_setup

---
+++ src/js/components/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Header extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            addMovieInput: ""
+        }
+    }
+    render(){
+        return <div>Hello World</div>
+    }
+
+  
+}
+
+export {Header}
---
+++ src/js/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Header} from './components/header.jsx'
+
+// require('../sass/main.scss');
+
+ReactDOM.render(
+    <Header/>,
+    document.getElementById('app')
+);
