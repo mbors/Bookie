73775a98a4829058b9dd0ff33d9e7510cb510873
Martyna Bors <martynabors@gmail.com>
2017-11-22 09:22:13 +0100

    NYtimes_api_v1

--- src/js/components/bookie.jsx
+++ src/js/components/bookie.jsx
@@ -79,7 +79,7 @@ class BookieHiddenNav extends React.Component {
             <header className="main-header">
        <div className="container">
             <h1 className="main-logo">
-                <span>Bookie</span>
+                <a href="#"><span>Bookie</span></a>
                 <a href="#">
                     <img className="first-unicorn" src="mock-ups/img/unicorn.png"/>
                     <img className="second-unicorn" src="mock-ups/img/unicorn2.png"/>
@@ -99,23 +99,74 @@ class BookieHiddenNav extends React.Component {
  }
 
 
+class BookieNews extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            headlineTab: [],
+            newsTab: [],
+            urlTab: [],
+        }
+    }
+
+    render(){ 
+        let mySection = []
+                for(let i=0; i<5; i++){
+                     mySection.push(
+                     <div className="article-content">
+                         <div className="article-title">{this.state.headlineTab[i]}</div>
+                        <p className="snippet">{this.state.newsTab[i]}</p> 
+                        <a href={this.state.urlTab[i]}>Tell me more!</a>
+                    </div>
+                    )
+            }
+            console.log(mySection)
+        return(
+            <div className="main-section">
+                <div className="container">
+                        <div className="section1">
+                            <h2 className="page-title">Bookie News<span> .</span></h2>
+                            {mySection}
+                        </div>
+                </div>
+            </div>
+        )
+
+    }
+    componentDidMount(){
+            fetch('https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=651f2d666d474593a7166bd2238e94aa&q=books')
+            .then(resp=>{
+                return resp.json();
+            })
+            .then(response=>{
+                let myHeadlines = [];
+                let myNews = [];
+                let myUrls = [];
+                for(let i=0; i<5; i++){
+                    myHeadlines.push(response.response.docs[i].headline.main)
+                    myNews.push( response.response.docs[i].snippet)
+                    myUrls.push(response.response.docs[i].web_url)
+                }
+                this.setState({
+                    headlineTab: myHeadlines,
+                    newsTab: myNews,
+                    urlTab: myUrls
+                })
+            })
+        }
+}
+
 class Bookie extends React.Component {
     render(){
         return  (
             <div>
             <Header/>
-    <div className="main-section">
-        <div className="container">
-             <div className="section1">
-                    Pie bear claw jelly-o. Dragée jelly macaroon macaroon ice cream. Powder dragée pie ice cream. Cheesecake pastry pie lemon drops gummies cake oat cake gummi bears lemon drops. Wafer pie tootsie roll marshmallow dragée gummies. Dragée candy canes fruitcake. Ice cream bear claw cotton candy powder tiramisu bonbon lemon drops. Sweet dessert gummies chupa chups cake muffin. Chocolate cake pie danish. Sugar plum lollipop chocolate cake croissant carrot cake. Cupcake halvah caramels bear claw sesame snaps jelly-o. Apple pie pastry oat cake soufflé donut dessert.
-                    Soufflé pie candy. Powder sweet brownie lemon drops fruitcake pie carrot cake chocolate. Carrot cake chocolate tiramisu. Chocolate cake oat cake sweet fruitcake topping. Danish jelly-o chocolate pastry wafer. Jelly-o macaroon tiramisu chocolate pudding candy macaroon carrot cake soufflé. Cheesecake gummies pie danish cotton candy cotton candy tart. Tart candy sweet topping apple pie. Ice cream pie danish sweet dragée sweet roll pudding. Liquorice jelly beans cheesecake chocolate sesame snaps cupcake. Sesame snaps gummies gummies. Pudding pastry toffee cupcake fruitcake sesame snaps chocolate cake. Icing jelly-o ice cream soufflé.
-             </div>
-        </div>
-    </div>
+            <BookieNews/>
     <footer className="main-footer">
         <div className="container">
             <div className="bookie-map">
                 <a href="#">About Bookie</a>
+                <a href="#"><img src="mock-ups/img/png/internet.png"/></a>
             </div>
             <div className="social-media">
                     <a href="#"><img src="mock-ups/img/png/facebook.png"/></a>
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Header} from './components/header.jsx'
+import {Bookie} from './components/bookie.jsx'
 
 // require('../sass/main.scss');
 
 ReactDOM.render(
-    <Header/>,
+    <Bookie/>,
     document.getElementById('app')
 );
