7b43961a001927e9799460d098022345507184e9
Martyna Bors <martynabors@gmail.com>
2017-11-23 09:54:48 +0100

    refactoring

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import {Bookie} from './components/bookie.jsx'
 import {Routing} from './components/routing.jsx'
 
-=======
-import {Routing} from './routing.jsx'
-// import {Bookie} from './components/bookie/bookie.jsx'
-// import {Future} from './components/bookiebooks/future.jsx'
->>>>>>> 600d7e02ec9079c090653cf4536836e60759eb2c
-
 
 // require('../sass/main.scss');
+
 ReactDOM.render(
-    <Routing/>,
+    <Bookie/>,
     document.getElementById('app')
 );
